cf34953eeee2267c38624397cb44aa7dd88eccf0
mateenOlalekan <Xolablack@1ne>
2024-04-25 21:19:36 +0100

    First Commit

--- src/Component/Dashboard/SideBar.jsx
+++ src/Component/Dashboard/SideBar.jsx
@@ -1,9 +1,115 @@
-import React from 'react'
+import { LightIcon, TeamsIcon, BillingIcon, InvoicesIcon, AccountIcon, LogoutIcon } from './ReactionIcons';
 
-function SideBar() {
+function Sidebar() {
   return (
-    <div>SideBar</div>
-  )
+    <div className="flex h-screen w-16 flex-col justify-between border-e bg-white">
+      <div>
+        <div className="inline-flex size-16 items-center justify-center">
+          <span className="grid size-10 place-content-center rounded-lg bg-gray-100 text-xs text-gray-600">
+            L
+          </span>
+        </div>
+
+        <div className="border-t border-gray-100">
+          <div className="px-2">
+            <div className="py-4">
+              <a
+                href="#"
+                className="t group relative flex justify-center rounded bg-blue-50 px-2 py-1.5 text-blue-700"
+              >
+                <LightIcon className="size-5 opacity-75" />
+
+                <span
+                  className="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible"
+                >
+                  General
+                </span>
+              </a>
+            </div>
+
+            <ul className="space-y-1 border-t border-gray-100 pt-4">
+              <li>
+                <a
+                  href="#"
+                  className="group relative flex justify-center rounded px-2 py-1.5 text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+                >
+                  <TeamsIcon className="size-5 opacity-75" />
+
+                  <span
+                    className="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible"
+                  >
+                    Teams
+                  </span>
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="group relative flex justify-center rounded px-2 py-1.5 text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+                >
+                  <BillingIcon className="size-5 opacity-75" />
+
+                  <span
+                    className="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible"
+                  >
+                    Billing
+                  </span>
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="group relative flex justify-center rounded px-2 py-1.5 text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+                >
+                  <InvoicesIcon className="size-5 opacity-75" />
+
+                  <span
+                    className="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible"
+                  >
+                    Invoices
+                  </span>
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className="group relative flex justify-center rounded px-2 py-1.5 text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+                >
+                  <AccountIcon className="size-5 opacity-75" />
+
+                  <span
+                    className="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible"
+                  >
+                    Account
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="sticky inset-x-0 bottom-0 border-t border-gray-100 bg-white p-2">
+        <form action="#">
+          <button
+            type="submit"
+            className="group relative flex w-full justify-center rounded-lg px-2 py-1.5 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+          >
+            <LogoutIcon className="size-5 opacity-75" />
+
+            <span
+              className="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible"
+            >
+              Logout
+            </span>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default SideBar
+export default Sidebar
